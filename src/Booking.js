469555248e71bfa26d538746d79afcad261433e9
Jason Ankers <ankers261@gmail.com>
2022-05-20 17:43:15 +0100

    Booking development

--- src/Booking.js
+++ src/Booking.js
@@ -1,14 +1,50 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+
+
+
 
 function Booking() {
+
+    const [barbers, setBarbers] = useState(null);
+
+    
+    useEffect(() => {
+        fetch('http://localhost:9999/staff')
+            .then(res => {
+               return res.json();
+            })
+            .then(data => {
+                setBarbers(data);
+                console.log(data);
+                todaysDate();
+            });
+    }, []);
+    
+    const currentDate = new Date();
+
+    const [date, setDate] = useState();
+
+    function todaysDate() {
+        setDate(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+    }
+
+
     return (
         <div className='booking'>
-            <h2>Create a booking</h2>
-            <p>This will be a calendar when implemented</p>
-            <p>This will be a dropdown list to select an available time</p>
-            <p>This will be a dropdown list to select the style of haircut</p>
-            <p>This will display the price of the haircut</p>
-            <button>Confirm Booking</button>
+            <div className='bookingForm'>
+                <form action = "http://localhost:9999/staffAvailability" method = "POST">
+                    <label htmlFor = "barbers">Select a barber: </label>
+                        <select name = "barbers" id = "barbers">
+                            {barbers && barbers.map((barber) => (
+                                <option value = {barber.staffID} key = {barber.staffID}>{barber.fName} {barber.sName}</option>
+                            ))}
+                        </select>
+                    <label htmlFor = "dates">Select a date: </label>
+                        <input type = "date" min = {date}></input>
+                    <input type = "submit" value = "submit"></input>
+                </form>
+            </div>
         </div>
     );
 }
